faa2058fa406ef82c60393f71e94685ade9f3e40
Soluchi Chine <soluchichine0803@gmail.com>
2026-06-19 17:14:16 +0100

    backend fix 2

--- server/src/routes/planRoutes.ts
+++ server/src/routes/planRoutes.ts
@@ -1,11 +1,7 @@
 import { Router } from 'express';
 import { getPlans, getPlanById, getAllPlans } from '../controllers/planController';
-import { authenticate } from '../middleware/authMiddleware';
 
 const router = Router();
-
-// All plan routes require authentication
-router.use(authenticate as any);
 
 router.get('/', getPlans);
 router.get('/all', getAllPlans);
